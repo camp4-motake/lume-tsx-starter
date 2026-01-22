abc320c6db553734e8f1507998d8ffbcb48bea26
motake <munetoshi_otake@camp4.jp>
2026-01-22 09:42:45 +0900

    refactor: fix attributes

--- src/_components/layouts/Footer/comp.tsx
+++ src/_components/layouts/Footer/comp.tsx
@@ -4,14 +4,11 @@ export default function (
   { ...props }: Lume.Data,
   { useAttrs }: Lume.Helpers,
 ) {
-  const attributes = { ...useAttrs(props, ["content"]) };
+  const attributes = useAttrs(props, "footer");
   const year = new Date().getFullYear();
 
   return (
-    <footer
-      {...{ ...attributes }}
-      class={clsx("footer", props?.class)}
-    >
+    <footer {...{ ...attributes }} class={clsx("footer", props?.class)}>
       <p class="copyright">
         <small>Copyright &copy; {year} corp name</small>
       </p>
--- src/_components/layouts/Header/comp.tsx
+++ src/_components/layouts/Header/comp.tsx
@@ -4,13 +4,10 @@ export default function (
   { title, ...props }: Lume.Data,
   { useAttrs }: Lume.Helpers,
 ) {
-  const attributes = { ...useAttrs(props, ["content"]) };
+  const attributes = useAttrs(props, "header");
 
   return (
-    <header
-      {...{ ...attributes }}
-      class={clsx("header", props?.class)}
-    >
+    <header {...{ ...attributes }} class={clsx("header", props?.class)}>
       <a href="/">{title}</a>
     </header>
   );
