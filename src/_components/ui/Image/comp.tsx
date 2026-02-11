5dfa8351556c6adec5fe6d63a54975b8655c0bb3
motake <munetoshi_otake@camp4.jp>
2026-02-11 18:06:17 +0900

    add img components

---
+++ src/_components/ui/Image/comp.tsx
@@ -0,0 +1,41 @@
+/**
+ * Auto Image Component
+ *
+ * @see https://lume.land/plugins/image_size/
+ * @see https://lume.land/plugins/picture/
+ * @see https://lume.land/plugins/transform_images/
+ */
+
+interface Props {
+  sources?: {
+    srcset: string;
+    media?: string;
+  }[];
+}
+
+export default function Image(
+  { sources = [], ...props }: Props & Lume.Data,
+  { useAttrs }: Lume.Helpers,
+) {
+  const attributes = {
+    "image-size": "", // @see https://lume.land/plugins/image_size/
+    "transform-images": "avif png", // @see https://lume.land/plugins/picture/
+    loading: "lazy",
+    ...useAttrs(props, "img"),
+  };
+
+  if (sources.length > 0) {
+    return (
+      <picture>
+        {sources.map((source: { srcset: string }) =>
+          source?.srcset && (
+            <source key={source.srcset} transform-images="avif png" {...{ ...source }} />
+          )
+        )}
+        <img {...{ ...attributes }} />
+      </picture>
+    );
+  }
+
+  return <img {...{ ...attributes }} />;
+}
--- src/index.page.tsx
+++ src/index.page.tsx
@@ -3,22 +3,27 @@ export const title = "example";
 export const description = "";
 
 export default ({ comp }: Lume.Data) => {
-  const { Button } = comp.ui;
+  const { Button, Image } = comp.ui;
 
   return (
     <div>
       <h1>Hello world!</h1>
+
+      {/* default */}
       <figure>
-        <picture>
-          <source srcset="/assets/img/test300x300.png" />
-          <img
-            src="/assets/img/test300x300.png"
-            loading="lazy"
-            transform-images="avif png"
-            image-size
-          />
-        </picture>
+        <Image src="/assets/img/test300x300.png" loading="eager" />
       </figure>
+
+      {/* with sources */}
+      <figure>
+        <Image
+          src="/assets/img/test300x300.png"
+          sources={[
+            { srcset: "/assets/img/test300x300.png", media: "(min-width: 48em)" },
+          ]}
+        />
+      </figure>
+
       <div>
         <Button id="test-btn" href="#" class="sample-btn" title="sample button">
           component sample
